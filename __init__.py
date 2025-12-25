9ae5d5d56da7c4feec1fc29345c36e3ce36f9f24
Bob Swift <bswift@rsds.ca>
2025-12-25 12:15:56 -0700

    Refactor translations

--- __init__.py
+++ __init__.py
@@ -130,75 +130,18 @@ class GenreMapper:
 class GenreMapperOptionsPage(OptionsPage):
 
     TITLE = t_("ui.options_page_title", "Genre Mapper")
+    HELP_URL = USER_GUIDE_URL
 
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
         self.ui = Ui_GenreMapperOptionsPage()
         self.ui.setupUi(self)
-        self._add_translations()
-
-    def _add_translations(self):
-        bold_start = '<span style="font-weight:600;">'
-        bold_end = '</span>'
-
-        self.ui.gm_description.setTitle(self.api.tr("ui.gm_description", "Genre Mapper"))
-        self.ui.format_description.setText(
-            "<html><head/><body><p>"
-            + self.api.tr(
-                "ui.format_description_p1",
-                (
-                    "These are the original / replacement pairs used to map one genre entry to another. Each pair must be entered on a "
-                    "separate line in the form:"
-                )
-            )
-            + "</p><p>"
-            + bold_start
-            + self.api.tr("ui.format_description_p2", "[genre match test string]=[replacement genre]")
-            + bold_end
-            + "</p><p>"
-            + self.api.tr(
-                "ui.format_description_p3",
-                (
-                    "Unless the \"regular expressions\" option is enabled, supported wildcards in the test string part of the mapping include "
-                    "'*' and '?' to match any number of characters and a single character respectively. An example for mapping all types of "
-                    "Rock genres (e.g. Country Rock, Hard Rock, Progressive Rock) to \"Rock\" would be done using the following line:"
-                )
-            )
-            + "</p><p>"
-            + "<span style=\" font-family:\'Courier New\'; font-size:10pt; font-weight:600;\">"
-            + self.api.tr("ui.format_description_p4", "*rock*=Rock")
-            + "</span></p><p>"
-            + self.api.tr(
-                "ui.format_description_p5",
-                (
-                    "Blank lines and lines beginning with an equals sign (=) will be ignored. Case-insensitive tests are used when matching. "
-                    "Replacements will be made in the order they are found in the list."
-                )
-            )
-            + "</p><p>"
-            + self.api.tr(
-                "ui.format_description_p6",
-                "Please see the {start_link}User Guide{end_link} for additional information."
-            ).format(start_link=f'<a href="{USER_GUIDE_URL}"><span style="text-decoration: underline; color:#0000ff;">', end_link="</span></a>")
-            + "</p></body></html>"
-        )
-        self.ui.cb_enable_genre_mapping.setText(self.api.tr("ui.cb_enable_genre_mapping", "Enable genre mapping"))
-        self.ui.gm_replacement_pairs.setTitle(self.api.tr("ui.gm_replacement_pairs", "Replacement Pairs"))
-        self.ui.cb_use_regex.setText(self.api.tr("ui.cb_use_regex", "Match tests are entered as regular expressions"))
-        self.ui.genre_mapper_first_match_only.setText(self.api.tr("ui.genre_mapper_first_match_only", "Apply only the first matching replacement"))
-        self.ui.genre_mapper_replacement_pairs.setPlaceholderText(self.api.tr("ui.genre_mapper_replacement_pairs", "Enter replacement pairs (one per line)"))
 
     def load(self):
-        # Enable external link
-        self.ui.format_description.setOpenExternalLinks(True)
-
         self.ui.genre_mapper_replacement_pairs.setPlainText(self.api.plugin_config[OPT_MATCH_PAIRS])
         self.ui.genre_mapper_first_match_only.setChecked(self.api.plugin_config[OPT_MATCH_FIRST])
         self.ui.cb_enable_genre_mapping.setChecked(self.api.plugin_config[OPT_MATCH_ENABLED])
         self.ui.cb_use_regex.setChecked(self.api.plugin_config[OPT_MATCH_REGEX])
-
-        self.ui.cb_enable_genre_mapping.stateChanged.connect(self._set_enabled_state)
-        self._set_enabled_state()
 
     def save(self):
         self.api.plugin_config[OPT_MATCH_PAIRS] = self.ui.genre_mapper_replacement_pairs.toPlainText()
@@ -207,9 +150,6 @@ class GenreMapperOptionsPage(OptionsPage):
         self.api.plugin_config[OPT_MATCH_REGEX] = self.ui.cb_use_regex.isChecked()
 
         GenreMapper(self.api).refresh()
-
-    def _set_enabled_state(self, *args):
-        self.ui.gm_replacement_pairs.setEnabled(self.ui.cb_enable_genre_mapping.isChecked())
 
 
 def enable(api: PluginApi):
